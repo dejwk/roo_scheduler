9182261a7e51df6033278ae9c11a7e0d899a4094
dejwk <dawid.kurzyniec@gmail.com>
2023-12-31 15:00:29 +0100

    Added cancellation tests.

--- .testing/test/roo_scheduler_test.cpp
+++ .testing/test/roo_scheduler_test.cpp
@@ -174,11 +174,6 @@ struct TestTask : public Executable {
   std::vector<ExecutionID>& observed;
 };
 
-struct Experiment {
-  int64_t micros;
-  ExecutionID id;
-};
-
 TEST(Scheduler, StableScheduleOrder) {
   Scheduler scheduler;
   std::vector<ExecutionID> observed;
@@ -205,7 +200,12 @@ TEST(Scheduler, LargeRandomTest) {
 
   TestTask test(observed);
 
+  struct Experiment {
+    int64_t micros;
+    ExecutionID id;
+  };
   std::vector<Experiment> expected;
+
   for (int i = 0; i < 10000; ++i) {
     int64_t micros = rand() % 2000000;
     ExecutionID id = scheduler.scheduleAfter(&test, Micros(micros));
@@ -224,6 +224,100 @@ TEST(Scheduler, LargeRandomTest) {
     ++i;
   }
   ASSERT_EQ(i, 10000);
+}
+
+TEST(Scheduler, LargeRandomCancellationTest) {
+  Scheduler scheduler;
+  std::vector<ExecutionID> observed;
+
+  TestTask test(observed);
+
+  struct Experiment {
+    int64_t micros;
+    ExecutionID id;
+    bool cancelled;
+  };
+  std::vector<Experiment> expected;
+
+  for (int i = 0; i < 10000; ++i) {
+    int64_t micros = rand() % 2000000;
+    ExecutionID id = scheduler.scheduleAfter(&test, Micros(micros));
+    expected.push_back(
+        Experiment{.micros = micros, .id = id, .cancelled = false});
+  }
+  for (int i = 0; i < 2000; ++i) {
+    int64_t pos = rand() % 10000;
+    scheduler.cancel(expected[pos].id);
+    expected[pos].cancelled = true;
+  }
+  expected.erase(
+      std::remove_if(expected.begin(), expected.end(),
+                     [](const Experiment& e) { return e.cancelled; }),
+      expected.end());
+
+  std::sort(expected.begin(), expected.end(),
+            [](const Experiment& a, const Experiment& b) {
+              return a.micros < b.micros ||
+                     (a.micros == b.micros && a.id < b.id);
+            });
+  delay(Seconds(2));
+  int i = 0;
+  while (!scheduler.executeEligibleTasks(1)) {
+    EXPECT_EQ(observed[i], expected[i].id)
+        << "at " << i << "; expected time: " << expected[i].micros;
+    ++i;
+  }
+  ASSERT_EQ(i, expected.size());
+}
+
+TEST(Scheduler, LargeRandomCancellationTestWithPruning) {
+  Scheduler scheduler;
+  std::vector<ExecutionID> observed;
+
+  TestTask test(observed);
+
+  struct Experiment {
+    int64_t micros;
+    ExecutionID id;
+    bool cancelled;
+  };
+  std::vector<Experiment> expected;
+
+  for (int i = 0; i < 10000; ++i) {
+    int64_t micros = rand() % 2000000;
+    ExecutionID id = scheduler.scheduleAfter(&test, Micros(micros));
+    expected.push_back(
+        Experiment{.micros = micros, .id = id, .cancelled = false});
+  }
+  for (int i = 0; i < 1000; ++i) {
+    int64_t pos = rand() % 10000;
+    scheduler.cancel(expected[pos].id);
+    expected[pos].cancelled = true;
+  }
+  scheduler.pruneCanceled();
+  for (int i = 0; i < 1000; ++i) {
+    int64_t pos = rand() % 10000;
+    scheduler.cancel(expected[pos].id);
+    expected[pos].cancelled = true;
+  }
+  expected.erase(
+      std::remove_if(expected.begin(), expected.end(),
+                     [](const Experiment& e) { return e.cancelled; }),
+      expected.end());
+
+  std::sort(expected.begin(), expected.end(),
+            [](const Experiment& a, const Experiment& b) {
+              return a.micros < b.micros ||
+                     (a.micros == b.micros && a.id < b.id);
+            });
+  delay(Seconds(2));
+  int i = 0;
+  while (!scheduler.executeEligibleTasks(1)) {
+    EXPECT_EQ(observed[i], expected[i].id)
+        << "at " << i << "; expected time: " << expected[i].micros;
+    ++i;
+  }
+  ASSERT_EQ(i, expected.size());
 }
 
 }  // namespace roo_scheduler
